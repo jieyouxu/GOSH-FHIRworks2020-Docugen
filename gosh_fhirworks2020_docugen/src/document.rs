236ad468bbd93c50a26bdf5b7cac29755f6ca2c9
许杰友 Jieyou Xu (Joe) <jieyouxu@outlook.com>
2020-03-02 18:04:59 +0000

    add base case

--- gosh_fhirworks2020_docugen/src/document.rs
+++ gosh_fhirworks2020_docugen/src/document.rs
@@ -5,10 +5,24 @@ pub(crate) struct DocumentTemplate {
     pub(crate) partials: Vec<Partial>,
 }
 
+impl DocumentTemplate {
+    fn new() -> Self {
+        Self {
+            partials: Vec::new(),
+        }
+    }
+
+    fn with_partials(partials: &[Partial]) -> Self {
+        Self {
+            partials: partials.to_vec(),
+        }
+    }
+}
+
 pub(crate) type Identifier = String;
 
 /// Each `Partial` is either a UTF-8 `StringLiteral`, or a `Tag`.
-#[derive(Debug, PartialEq)]
+#[derive(Debug, PartialEq, Clone)]
 pub(crate) enum Partial {
     StringLiteral(String),
     Tag(Identifier),
@@ -26,8 +40,10 @@ pub(crate) struct TagPair {
     pub(crate) value: String,
 }
 
+/// Cause of error when trying to fill a `DocumentTemplate`.
 pub(crate) enum TemplateError {
     MissingRequiredTagValue(Identifier),
+    NonExhaustiveTags(Vec<Identifier>),
 }
 
 impl DocumentTemplate {
@@ -37,27 +53,45 @@ impl DocumentTemplate {
     ) -> Result<FilledDocument, TemplateError> {
         let mut content = String::new();
 
+        // TODO: replace this `O(n^2)` loop with a `O(1)` `HashMap`. Currently
+        // this requires iterating over `self.partials` in the outer loop and
+        // iterating over `tag_pairs` in the inner loop in the worst case
+        // scenario.
         for partial in &self.partials[..] {
             match partial {
                 Partial::StringLiteral(s) => content.push_str(s),
                 Partial::Tag(id) => {
-                    let tag_value =
-                        match tag_pairs.iter().find(|&t| &t.key == id) {
-                            Some(TagPair { value, .. }) => value,
-                            None => {
-                                return Err(
-                                    TemplateError::MissingRequiredTagValue(
-                                        id.to_string(),
-                                    ),
-                                );
-                            }
-                        };
-
+                    let tag_value = saturate_or_error(tag_pairs, id)?;
                     content.push_str(tag_value);
                 }
             }
         }
 
         Ok(FilledDocument(content))
+    }
+}
+
+fn saturate_or_error<'a>(
+    tag_pairs: &'a [TagPair],
+    tag_key: &'a str,
+) -> Result<&'a str, TemplateError> {
+    match tag_pairs.iter().find(|&t| &t.key == tag_key) {
+        Some(TagPair { value, .. }) => Ok(value),
+        None => {
+            Err(TemplateError::MissingRequiredTagValue(tag_key.to_string()))
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_no_tags() -> Result<(), String> {
+        let template = DocumentTemplate::new();
+        let saturated = template.saturate(&[]);
+        assert!(saturated.is_ok());
+        Ok(())
     }
 }
