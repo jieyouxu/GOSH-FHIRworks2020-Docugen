15f95bcb659adeca8a638f4c7f8081c43e999eb4
许杰友 Jieyou Xu (Joe) <jieyouxu@outlook.com>
2020-03-02 17:27:38 +0000

    remove unused import

--- gosh_fhirworks2020_docugen/src/parser.rs
+++ gosh_fhirworks2020_docugen/src/parser.rs
@@ -2,7 +2,7 @@ use pom::char_class::*;
 use pom::parser::*;
 use pom::Parser;
 
-use crate::document::{DocumentTemplate, Partial};
+use crate::document::{Partial};
 
 /// A `StringLiteral` parser combinator is responsible for parsing the following
 /// fragment:
